245d31e4add0f0256aa3acf4dff219d34c60d1e0
alejandro-reyesamaro <ale.uh.cu@gmail.com>
2023-11-28 20:04:58 +0100

    add cross origin annotation for controllers

--- src/main/java/com/harvest/api/controllers/AccountController.java
+++ src/main/java/com/harvest/api/controllers/AccountController.java
@@ -12,6 +12,7 @@ import com.harvest.core.entities.Account;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 
 
-//@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "https://localhost:44340")
 @RestController
 @RequestMapping("/account")
 public class AccountController {
